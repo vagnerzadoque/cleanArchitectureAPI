45d0ff39f6c816a4905fe144d60eb0ecc7c70174
Vagner Barbosa <four004@einstein.br>
2021-06-10 01:21:31 -0300

    feat: ensure SignUpController returns 400 teste

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,18 @@
+
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpResquest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpResquest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
